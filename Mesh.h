190fee36954a861f315aef27177a73616d293c4e
IncharaPM <incharapm11@gmail.com>
2021-02-23 16:47:35 +0000

    Plane

---
+++ Mesh.h
@@ -0,0 +1,320 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+
+#include"Vertex.h"
+#include"Primitive.h"
+#include"Shader.h"
+#include"Texture.h"
+#include"Material.h"
+#include<gtc\matrix_transform.hpp>
+#include<gtc\type_ptr.hpp>
+#include <glm.hpp>
+#include<gtx\euler_angles.hpp>
+#include <gtx\euler_angles.hpp>
+#include <gtc\quaternion.hpp>
+#include <gtx\quaternion.hpp>
+#include <gtc\type_ptr.hpp>
+#include<gtc\quaternion.hpp>
+#include "maths_funcs.h"
+
+class Mesh
+{
+private:
+	Vertex * vertexArray;
+	unsigned nrOfVertices;
+	GLuint* indexArray;
+	unsigned nrOfIndices;
+
+	GLuint VAO;
+	GLuint VBO;
+	GLuint EBO;
+
+	glm::vec3 position;
+	glm::vec3 origin;
+	glm::vec3 rotation;
+	glm::vec3 scale;
+	float yaw;
+	float pitch;
+	float roll;
+	float qangle;
+	glm::vec3 angle;
+	glm::vec3 qaxis;
+
+	glm::mat4 ModelMatrix;
+
+	void initVAO()
+	{
+		//Create VAO
+		glCreateVertexArrays(1, &this->VAO);
+		glBindVertexArray(this->VAO);
+
+		//GEN VBO AND BIND AND SEND DATA
+		glGenBuffers(1, &this->VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+		glBufferData(GL_ARRAY_BUFFER, this->nrOfVertices * sizeof(Vertex), this->vertexArray, GL_STATIC_DRAW);
+
+		//GEN EBO AND BIND AND SEND DATA
+		if (this->nrOfIndices > 0)
+		{
+			glGenBuffers(1, &this->EBO);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->nrOfIndices * sizeof(GLuint), this->indexArray, GL_STATIC_DRAW);
+		}
+
+		//SET VERTEXATTRIBPOINTERS AND ENABLE (INPUT ASSEMBLY)
+		//Position
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, position));
+		glEnableVertexAttribArray(0);
+		//Color
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, color));
+		glEnableVertexAttribArray(1);
+		//Texcoord
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, texcoord));
+		glEnableVertexAttribArray(2);
+		//Normal
+		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, normal));
+		glEnableVertexAttribArray(3);
+
+		//BIND VAO 0
+		glBindVertexArray(0);
+	}
+
+	void updateUniforms(Shader* shader)
+	{
+		shader->setMat4fv(this->ModelMatrix, "ModelMatrix");
+	}
+
+	void updateModelMatrix()
+	{
+		this->ModelMatrix = glm::mat4(1.f);
+		this->ModelMatrix = glm::translate(this->ModelMatrix, this->origin);
+		this->ModelMatrix = glm::rotate(this->ModelMatrix, glm::radians(this->rotation.x), glm::vec3(1.f, 0.f, 0.f));
+		this->ModelMatrix = glm::rotate(this->ModelMatrix, glm::radians(this->rotation.y), glm::vec3(0.f, 1.f, 0.f));
+		this->ModelMatrix = glm::rotate(this->ModelMatrix, glm::radians(this->rotation.z), glm::vec3(0.f, 0.f, 1.f));
+		this->ModelMatrix = glm::translate(this->ModelMatrix, this->position - this->origin);
+		this->ModelMatrix = glm::scale(this->ModelMatrix, this->scale);
+	}
+
+	void updateModelMatrixForYPR(float yaw, float pitch, float roll)
+	{
+		glm::mat4 R = glm::mat4(1.0f);
+		R = glm::eulerAngleXYZ(glm::radians(yaw), glm::radians(pitch), glm::radians(roll));
+		this->ModelMatrix = this->ModelMatrix * R;
+	}
+
+	void updatemodelMatrixQ(float qangle, glm::vec3 qaxis)
+	{
+		versor local_quat = versor();
+		local_quat = quat_from_axis_deg(qangle, qaxis.x, qaxis.y, qaxis.z);
+		mat4 quat = quat_to_mat4(local_quat);
+		glm::mat4 temp = glm::mat4(quat.m[0], quat.m[1], quat.m[2], quat.m[3], quat.m[4], quat.m[5], quat.m[6], quat.m[7],
+			quat.m[8], quat.m[9], quat.m[10], quat.m[11], quat.m[12], quat.m[13], quat.m[14], quat.m[15]);
+		this->ModelMatrix = this->ModelMatrix * temp;
+	}
+
+public:
+	Mesh(
+		Vertex* vertexArray,
+		const unsigned& nrOfVertices,
+		GLuint* indexArray,
+		const unsigned& nrOfIndices,
+		glm::vec3 position = glm::vec3(0.f),
+		glm::vec3 origin = glm::vec3(0.f),
+		glm::vec3 rotation = glm::vec3(0.f),
+		glm::vec3 scale = glm::vec3(1.f))
+	{
+		this->position = position;
+		this->origin = origin;
+		this->rotation = rotation;
+		this->scale = scale;
+
+		this->nrOfVertices = nrOfVertices;
+		this->nrOfIndices = nrOfIndices;
+
+		this->vertexArray = new Vertex[this->nrOfVertices];
+		for (size_t i = 0; i < nrOfVertices; i++)
+		{
+			this->vertexArray[i] = vertexArray[i];
+		}
+
+		this->indexArray = new GLuint[this->nrOfIndices];
+		for (size_t i = 0; i < nrOfIndices; i++)
+		{
+			this->indexArray[i] = indexArray[i];
+		}
+
+		this->initVAO();
+		this->updateModelMatrix();
+	}
+
+	Mesh(
+		Primitive* primitive,
+		glm::vec3 position = glm::vec3(0.f),
+		glm::vec3 origin = glm::vec3(0.f),
+		glm::vec3 rotation = glm::vec3(0.f),
+		glm::vec3 scale = glm::vec3(1.f))
+	{
+		this->position = position;
+		this->origin = origin;
+		this->rotation = rotation;
+		this->scale = scale;
+
+		this->nrOfVertices = primitive->getNrOfVertices();
+		this->nrOfIndices = primitive->getNrOfIndices();
+
+		this->vertexArray = new Vertex[this->nrOfVertices];
+		for (size_t i = 0; i < this->nrOfVertices; i++)
+		{
+			this->vertexArray[i] = primitive->getVertices()[i];
+		}
+
+		this->indexArray = new GLuint[this->nrOfIndices];
+		for (size_t i = 0; i < this->nrOfIndices; i++)
+		{
+			this->indexArray[i] = primitive->getIndices()[i];
+		}
+
+		this->initVAO();
+		this->updateModelMatrix();
+	}
+
+	Mesh(const Mesh& obj)
+	{
+		this->position = obj.position;
+		this->origin = obj.origin;
+		this->rotation = obj.rotation;
+		this->scale = obj.scale;
+
+		this->nrOfVertices = obj.nrOfVertices;
+		this->nrOfIndices = obj.nrOfIndices;
+
+		this->vertexArray = new Vertex[this->nrOfVertices];
+		for (size_t i = 0; i < this->nrOfVertices; i++)
+		{
+			this->vertexArray[i] = obj.vertexArray[i];
+		}
+
+		this->indexArray = new GLuint[this->nrOfIndices];
+		for (size_t i = 0; i < this->nrOfIndices; i++)
+		{
+			this->indexArray[i] = obj.indexArray[i];
+		}
+
+		this->initVAO();
+		this->updateModelMatrix();
+	}
+
+	~Mesh()
+	{
+		glDeleteVertexArrays(1, &this->VAO);
+		glDeleteBuffers(1, &this->VBO);
+
+		if (this->nrOfIndices > 0)
+		{
+			glDeleteBuffers(1, &this->EBO);
+		}
+
+		delete[] this->vertexArray;
+		delete[] this->indexArray;
+	}
+
+	//Accessors
+
+	//Modifiers
+	void setPosition(const glm::vec3 position)
+	{
+		this->position = position;
+	}
+
+	void setOrigin(const glm::vec3 origin)
+	{
+		this->origin = origin;
+	}
+
+	void setRotation(const glm::vec3 rotation)
+	{
+		this->rotation = rotation;
+	}
+
+	void setScale(const glm::vec3 setScale)
+	{
+		this->scale = scale;
+	}
+
+	//Functions
+
+	void move(const glm::vec3 position)
+	{
+		this->position += position;
+	}
+
+	void rotate(const glm::vec3 rotation)
+	{
+		this->rotation += rotation;
+	}
+
+	void scaleUp(const glm::vec3 scale)
+	{
+		this->scale += scale;
+	}
+
+	void update()
+	{
+
+	}
+
+	void EulerAngle(float yaw, float pitch, float roll)
+	{
+		this->yaw += yaw;
+		this->pitch += pitch;
+		this->roll += roll;
+	}
+
+	void QAngle(float qangle, const glm::vec3 qaxis)
+	{
+		if (qaxis.x == 1.0f)
+		{
+			this->angle += glm::vec3(5.0f, 0.0f, 0.0f);
+			this->qangle = angle.x;
+		}
+		else if (qaxis.y == 1.0f)
+		{
+			this->angle += glm::vec3(0.0f, 5.0f, 0.0f);
+			this->qangle = angle.y;
+		}
+		else if (qaxis.z == 1.0f)
+		{
+			this->angle += glm::vec3(0.0f, 0.0f, 5.0f);
+			this->qangle = angle.z;
+		}
+		this->qaxis = qaxis;
+	}
+
+	void render(Shader* shader)
+	{
+		//Update uniforms
+		this->updateModelMatrix();
+		this->updateModelMatrixForYPR(this->yaw, this->pitch, this->roll);
+		//this->updatemodelMatrixQ(this->qangle,this->qaxis);
+		this->updateUniforms(shader);
+
+		shader->use();
+
+		//Bind VAO
+		glBindVertexArray(this->VAO);
+
+		//RENDER
+		if (this->nrOfIndices == 0)
+			glDrawArrays(GL_TRIANGLES, 0, this->nrOfVertices);
+		else
+			glDrawElements(GL_TRIANGLES, this->nrOfIndices, GL_UNSIGNED_INT, 0);
+
+		//Cleanup
+		glBindVertexArray(0);
+		glUseProgram(0);
+		glActiveTexture(0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+};
